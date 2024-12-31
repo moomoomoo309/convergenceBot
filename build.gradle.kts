413820be147f3793386128a281cdcf12dced2351
Nicholas DeLello <nicholas_delello@yahoo.com>
2024-12-30 20:22:37 -0500

    Migrate to version catalogs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,6 @@
 
 import java.nio.file.Paths
 
-val antlr_version = "4.13.2"
-val argparse4j_version = "0.9.0"
-val coroutines_version = "1.10.1"
-val humanize_version = "0.1.2"
-val jackson_version = "2.18.2"
-val jda_version = "5.2.2"
-val logback_version = "1.5.15"
-val natty_version = "1.0.1"
-val pf4j_version = "3.12.0"
-
 plugins {
     val kotlin_version = "2.1.0"
     kotlin("jvm") version kotlin_version
@@ -39,22 +29,21 @@ buildscript {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.coroutines)
+    implementation(libs.kotlin.reflect)
 
-    implementation("io.github.natty-parser:natty:$natty_version")
-    implementation("net.sourceforge.argparse4j:argparse4j:$argparse4j_version")
-    implementation("to.lova.humanize:humanize-time:$humanize_version")
-    antlr("org.antlr:antlr4:$antlr_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation(libs.natty)
+    implementation(libs.argparse4j)
+    implementation(libs.humanize)
+    antlr(libs.antlr)
+    implementation(libs.logback)
 
-    implementation("net.dv8tion:JDA:$jda_version") {
+    implementation(libs.jda) {
         exclude(module = "opus-java")
     }
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jackson_version")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jackson_version")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jackson_version")
-
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.jsr310)
+    implementation(libs.jackson.kotlin)
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
